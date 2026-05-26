2a369ea46f00782a8491f14cc736687e6ba80e64
Celso-Badu-Gomes <celsobadugomes@gmail.com>
2026-05-26 11:30:17 -0300

    iniciando as implementacoes das classes

--- SistemaCadastro/src/app/Main.java
+++ SistemaCadastro/src/app/Main.java
@@ -1,7 +1,13 @@
 package app;
 
+import view.TelaCadastro;
+
 public class Main {
 	public static void main(String[] args) {
+		TelaCadastro telaCadastro = new TelaCadastro();
+		
 		System.out.println("Tela inicial");
+		telaCadastro.teste();
+		
 	}
 }
--- SistemaCadastro/src/view/TelaCadastro.java
+++ SistemaCadastro/src/view/TelaCadastro.java
@@ -2,4 +2,9 @@ package view;
 
 public class TelaCadastro {
 
+	public void teste() {
+		String teste = "ok";
+		System.out.println(teste);
+	}
+	
 }
