edb593c4a2a70e1700a3356458c4a35c004f9b1c
Celso-Badu-Gomes <celsobadugomes@gmail.com>
2026-05-26 15:39:46 -0300

    criacao do banco e instalacao das bibliotecas necessarias

--- SistemaCadastro/src/dao/ConnectionFactory.java
+++ SistemaCadastro/src/dao/ConnectionFactory.java
@@ -1,5 +1,29 @@
 package dao;
 
-public class ConnectionFactory {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class ConnectionFactory {
+	private static final String URL = "jdbc:sqlite:resources/produto.db";
+
+	public static Connection conectar() throws Exception {
+		Class.forName("org.sqlite.JDBC");
+		
+		if (connection == null || connection.isClosed()) {
+			connection = DriverManager.getConnection(URL);
+		}
+		return connection;
+	}
+
+	private static Connection connection = null;
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("Conectado com sucesso!!");
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}
+	}
 }
